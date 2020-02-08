4998fb518dec32691168eb33330b45e3b3649958
Lobsterdog Contributors <noreply@lobsterdog.dev>
2020-02-08 16:21:12 -0700

    Updating javascript to include Cypress

--- src/data/technologySkills/technologySkillsData.ts
+++ src/data/technologySkills/technologySkillsData.ts
@@ -4,7 +4,7 @@ export class TechnologySkillsData implements IBulletedListData {
   details: string[] = [
     'c#',
     'Java',
-    'Javascript (React, Angular, Protractor)',
+    'Javascript (React, Angular, Protractor, Cypress)',
     'Typescript',
     'SQL',
     'Selenium',
