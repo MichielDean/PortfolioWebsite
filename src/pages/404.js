7778cf8743e70b1137aceb4e5ac70f9a29d03c07
Lobsterdog Contributors <noreply@lobsterdog.dev>
2021-06-08 21:10:45 -0600

    Fixing a few image issues

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { withPrefix } from "gatsby"
 import loadable from '@loadable/component'
 import Wrapper from '../components/wrapper'
 import SEO from '../components/SEO'
@@ -13,11 +12,6 @@ const MainTitle = styled.h1`
   font-size: 3rem;
 `
 
-const Icon = styled.div`
-  width: 150px;
-  margin: 0 auto;
-`
-
 const Text = styled.p`
   text-align: center;
 `
@@ -27,9 +21,6 @@ const NotFoundPage = ({ location }) => (
     <SEO title="Page Not Found" />
     <Wrapper>
       <MainTitle>404 Page Not Found</MainTitle>
-      <Icon>
-        <img src={withPrefix('/images/ufo-and-cow.svg')} alt='ufo and cow' />
-      </Icon>
       <Text>
         Looks like you've followed a broken link or entered a URL that
         doesn't exist on this site.
